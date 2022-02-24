bd832b34c6a715e857f206474401783cca464846
yossep237 <yossep237@gmail.com>
2022-02-24 14:37:35 +0000

    add new medias and create new app

--- Porte_cle_connecte/Porte_cle_connecte.ino
+++ Porte_cle_connecte/Porte_cle_connecte.ino
@@ -3,23 +3,30 @@
  * placé sur un porte-clé lorsque l'utilisateur appuie 
  * sur un bouton depuis une application Android
  */
+#include <FastLED.h>
+#define NUM_LEDS 4
+#define DATA_PIN 12
 
- #include <ArduinoBLE.h>
- 
+// Declaration de variables
+int buzzerPin = 7;
+int ledPin = 4;
+int buttonPin = 8;
+char incomingValue = 0;
 
- // Declaration de variables
- int buzzerPin = 5;
- int ledPin = 4;
- int buttonPin = 8;
- char incomingValue = 0;
- 
+// Variables for leds
+CRGB leds[NUM_LEDS];
+
 void setup() {
   // Initialisation de la communication entre arduino et le module HC-05
   Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(100);
   pinMode(buzzerPin, OUTPUT);     // Definit la broche connecté au buzzer en mode sortie
   pinMode(ledPin, OUTPUT);        // Définit la broche connectée à la led en mode sortie
   pinMode(buttonPin, INPUT_PULLUP);   // Définit le type de péripherie pour le bouton
   digitalWrite(buttonPin, HIGH);
+  FastLED.clear();
+  drawLedOff();
  
 }
 
@@ -32,17 +39,38 @@ void loop() {
     if (incomingValue == '1')
     {
       digitalWrite(ledPin, HIGH);
-      digitalWrite(buzzerPin, HIGH);
+      buzzerAlarm();
+      drawLedOn();
       
     }
     // Lorsqu'il appuie sur OFF coupe le son et éteint la led
     else if(incomingValue == '0')
     {
-      digitalWrite(buzzerPin, LOW);
-      digitalWrite(ledPin, LOW); 
+      digitalWrite(ledPin, LOW);
+      drawLedOff(); 
     }
-
-    
   }
+}
 
+
+void drawLedOn() {
+  FastLED.clear();
+  for (int i=0; i< NUM_LEDS; i++) {
+    leds[i].setRGB(255,255,255) ;
+  }
+  FastLED.show();
+}
+
+void drawLedOff() {
+  FastLED.clear();
+  for (int i=0; i< NUM_LEDS; i++) {
+    leds[i].setRGB(0,0,0) ;
+  }
+  FastLED.show();
+}
+
+void buzzerAlarm() {
+  digitalWrite(buzzerPin, HIGH);
+  delay(150);
+  digitalWrite(buzzerPin, LOW);
 }
